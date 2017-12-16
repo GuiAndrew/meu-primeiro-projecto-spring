e5fc900065940706cb886b6c0092373af9082e07
GuiAndrew <guiandrew.pt@gmail.com>
2017-12-16 05:46:23 +0000

    Versão inicial do projecto.

---
+++ src/main/java/com/guiandrew/api/MeuPrimeiroProjectoApplication.java
@@ -0,0 +1,12 @@
+package com.guiandrew.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeuPrimeiroProjectoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeuPrimeiroProjectoApplication.class, args);
+	}
+}
---
+++ src/test/java/com/guiandrew/api/MeuPrimeiroProjectoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.guiandrew.api;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MeuPrimeiroProjectoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
